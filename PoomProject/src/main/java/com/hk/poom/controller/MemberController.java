8506a232da5ef73995c96ec9a9ba5705aa2f5726
hkedu <hkedu@192.168.4.123>
2020-09-29 16:12:16 +0900

    컨트롤러 ,jsp 기본통  -신준영

--- PoomProject/src/main/java/com/hk/poom/controller/MemberController.java
+++ PoomProject/src/main/java/com/hk/poom/controller/MemberController.java
@@ -1,8 +1,94 @@
 package com.hk.poom.controller;
 
+import java.util.Locale;
+
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
 
 @Controller
 public class MemberController {
+	
+	
+	@GetMapping("/poom/register/com")
+	public String registerCom( ) {
+		
+		
+		return "member/registerCom";
+	}
+	
+	@PostMapping("/poom/register/com")
+	public String registerComPost( ) {
+		
+		
+		return "member/registerComPost";
+	}
+	
+	
+	
+	@GetMapping("/poom/register/per")
+	public String registerPer( ) {
+		
+		
+		return "member/registerPer";
+	}
+	
+	
+	@GetMapping("/poom/register/new")
+	public String registerNew( ) {
+		
+		
+		return "member/registerNew";
+	}
+	
+	@PostMapping("/poom/register/new")
+	public String registerNewPost( ) {
+		
+		
+		return "member/registerNewPost";
+	}
+	
+	@GetMapping("/poom/login")
+	public String login( ) {
+		
+		
+		return "member/login";
+	}
+	
+	@PostMapping("/poom/login")
+	public String loginPost( ) {
+		
+		//로그인 성공시 홈으로
+		
+		//로그인 실패시
+		return "member/loginFail";
+	}
+	
+	@GetMapping("/poom/logout")
+	public String logout( ) {
+		
+		
+		return "member/logout";
+	}
+	
+	@GetMapping("/poom/find/id")
+	public String findId( ) {
+		
+		
+		return "member/findId";
+	}
+	
+	@GetMapping("/poom/find/pwd")
+	public String findPwd( ) {
+		
+		
+		return "member/findPwd";
+	}
+	
 
 }
