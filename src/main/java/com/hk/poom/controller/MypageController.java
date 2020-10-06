f302c64cf8a15dde89cc935f03c4d68967b7e826
hkedu <hkedu@192.168.4.123>
2020-10-06 09:15:19 +0900

    마이페이지 대문자 교체

--- src/main/java/com/hk/poom/controller/MypageController.java
+++ src/main/java/com/hk/poom/controller/MypageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class mypageController {
+public class MypageController {
 	
 	@GetMapping("/poom/mypage")
 	public String mypage( ) {
